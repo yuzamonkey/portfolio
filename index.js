ad20f6300c8ca162fdbebb60a64c59ddc7d6dbbd
Jusa Mankki <jusamankki@gmail.com>
2021-05-19 10:38:37 +0300

    add form properties

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function validate() {
+  console.log("Validate called")
+}
+
+function hadleSubmit() {
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  console.log("EMAIL", email)
+  console.log("MESSAGE", message)
+}
